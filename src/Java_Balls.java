394f8abfa9cd5856a7f269818930010dbe1bb993
Ben-Derhover <Oliver.james@talawanda.org>
2024-02-07 13:49:49 -0500

    lol

--- src/Java_Balls.java
+++ src/Java_Balls.java
@@ -1,6 +1,33 @@
 public class Java_Balls {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int operandA = 1;
+        int operandB = 3;
+        int Sum = operandA + operandB;
+        int Product = operandB * operandA;
+        int Difference= operandA - operandB;
+        int Quotient = operandA/operandB;
+        int Modulo = operandA%operandB;
+        double dblOperandA = 1.7;
+        double dblOperandB = 3.2;
+        double dblSum = dblOperandA + dblOperandB;
+        double dblProduct = dblOperandB *dblOperandA;
+        double dblDifference = dblOperandA-dblOperandB;
+        double dblQuotient = dblOperandA/dblOperandB;
+        double dblModulo= dblOperandA%dblOperandB;
+
+        System.out.println("sum: "+ Sum);
+        System.out.println("Product: "+ Product);
+        System.out.println("Difference :" + Difference);
+        System.out.println("Quotient: "+ Quotient);
+        System.out.println("Modulo: "+ Modulo);
+
+        System.out.println("dblSum: "+ dblSum);
+        System.out.println("dblProduct: "+ dblProduct);
+        System.out.println("dblDifference: "+ dblDifference);
+        System.out.println("dblQuotient: "+ dblQuotient);
+        System.out.println("dblModulo: "+ dblModulo);
+
+
     }
 
 
