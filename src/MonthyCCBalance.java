9e99de7a9fd7b16e1986ef9571d893f637621bdc
Ben-Derhover <Oliver.james@talawanda.org>
2024-02-08 13:47:24 -0500

    lol

---
+++ src/MonthyCCBalance.java
@@ -0,0 +1,17 @@
+public class MonthyCCBalance {
+    public static void main(String[] args) {
+
+        int initalBalance = 5000;
+        double interest = 1.17;
+        double month1 = initalBalance * interest;
+        double month2 = month1 * interest;
+
+        System.out.println("your first month payment is $" + month1);
+        System.out.println("your first Second payment is $" + month2);
+
+
+
+
+
+    }
+}
