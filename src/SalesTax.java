7bb2e5def3ecceb52c52cd07fc6763000f48f1d1
Ben-Derhover <Oliver.james@talawanda.org>
2024-02-08 13:35:21 -0500

    lol

---
+++ src/SalesTax.java
@@ -0,0 +1,12 @@
+public class SalesTax {
+    public static void main(String[] args) {
+    double salesTaxes = 0.05;
+    int intPrice = 15;
+    double totalPrice = salesTaxes * intPrice + intPrice;
+
+        System.out.println("your tax about is " + salesTaxes * intPrice);
+        System.out.println("your total is " + totalPrice);
+
+
+    }
+}
