3ec08a11845f55d05a43dde2a6bbef351ee9bd1e
Ben-Derhover <Oliver.james@talawanda.org>
2024-02-08 13:42:01 -0500

    lol

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,21 @@
+public class YearlyMaintenanceCost {
+    public static void main(String[] args) {
+        int spring = 2000;
+        int summer = 1500;
+        int fall = 2500;
+        int winter = 3000;
+        double total = spring + summer + fall + winter;
+
+        System.out.println("your spring cost is $" + spring);
+        System.out.println("your summer cost is $" + summer);
+        System.out.println("your fall cost is $" + fall);
+        System.out.println("your winter cost is $" + winter);
+        System.out.println("your total is $" + total);
+
+
+
+
+
+
+    }
+}
