0bfb1019f275ed158110c4ef3ddde0d3bc571c74
Ben-Derhover <Oliver.james@talawanda.org>
2024-02-08 13:51:04 -0500

    lol

---
+++ src/EvenOdd.java
@@ -0,0 +1,14 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num1 = 2;
+
+        if (num1%2 == 0){
+            System.out.println("your number is even");
+        }
+        else {
+            System.out.println("your numebr is not even");
+        }
+    }
+
+
+}
